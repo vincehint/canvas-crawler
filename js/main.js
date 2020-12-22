64cacde36d6338a7b5e672ec3e2f36ddf1a6ced6
Vince Hintermeister <vv.hintermeister@gmail.com>
2020-12-21 18:24:59 -0600

    new movement

--- js/main.js
+++ js/main.js
@@ -5,19 +5,70 @@ let movementDisplay = movement
 // in ye olden times we would say ; let game = document.getElementById('game')
 //Get some context
 
-game.setAttribute =('width', '800px')
-game.height = 400;
+game.setAttribute ('width', getComputedStyle(game)['width'])
+game.setAttribute ('height', getComputedStyle(game)['height'])
 
-
+//Get some context
 let ctx = game.getContext('2d')
 
-// Do sum dwrraring
-ctx.fillStyle = 'white'
-// fill color
-ctx.strokeStyle = 'red'
-//line color
-ctx.lineWidth = 5
-//line width
+function Crawler(x, y, color, width, height) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.width = width
+    this.height = height
+    this.alive = true
+    this.render = function() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
 
-ctx.fillRect(10, 10, 100, 100)
-ctx.strokeRect(10, 10, 100, 100)
+//move hero
+game.addEventListener('click', e => {
+    //want to draw the hero at offsetX and offsetY
+    ctx.clearRect(0, 0, game.width, game.height )
+    hero.x = e.offsetX
+    hero.y = e.offsetY
+    hero.render()
+})
+
+let hero = new Crawler(20, 20, 'purple', 20, 20)
+let ogre = new Crawler(400, 150, '#bada55', 60, 120)
+
+let gameLoop = () => {
+    
+    ctx.clearRect(0, 0, game.width, game.height)
+    movementDisplay.innerText = `X: ${hero.x}\nY: ${hero.y}`
+    if (ogre.alive) {
+
+        ogre.render()
+    }
+
+    hero.render()
+}
+
+let movementHandler = e => {
+    switch(e.key) {
+        case 'w':
+            // move up
+            hero.y -= 10
+            break
+        case 'a':
+            hero.x -= 10
+            break
+        case 's':
+            hero.y += 10
+            break
+        case 'd':
+            hero.x += 10
+            break
+        default:
+            console.log('that key has no purpose here')
+    }
+}
+
+document.addEventListener('keypress', movementHandler)
+
+
+let gameInterval = setInterval(gameLoop, 30)
